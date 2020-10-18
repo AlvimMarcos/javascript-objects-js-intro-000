de33d521f2b8192621c783c80089e2f0078c2442
Marcos Alvim <alvim.marcos@gmail.com>
2020-10-18 00:10:10 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,15 @@
+/*var meals ={}
+
+var meals = new Object(); */
+
+
+var playlist = {
+  metallica: "Fuel",
+  ozzy: "Paranoid",
+  guns: "Patience"
+}
+
+function updatePlaylist (playlist, artistName, songTitle ) {
+  playlist.skidRow = 'Eighteen';
+  return playlist();
+}
